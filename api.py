d8233888a64e09061604b075c8d76b2ea87ac3e6
Abdul13-m <asiddiqui@iom.int>
2025-01-15 12:31:42 +0500

    Update api.py

--- api.py
+++ api.py
@@ -58,4 +58,5 @@ def generate_link():
         return jsonify({"error": "Failed to generate the link"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8000)
+    app.run(debug=True)
+
